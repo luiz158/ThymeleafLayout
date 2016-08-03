99ded6cd49c77406ad60ca33faed3800ed999fd8
Fabiano Góes <fgoes@dellavolpe.com.br>
2016-08-03 11:45:35 -0300

    commit initial

---
+++ src/main/java/br/com/tdv/thymeleaflayout/ThymeleafLayoutApplication.java
@@ -0,0 +1,12 @@
+package br.com.tdv.thymeleaflayout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLayoutApplication.class, args);
+	}
+}
